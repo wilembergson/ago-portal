ee36a48a05d4fa622b242b50420fc1b9c58854b9
Wilembergson Alfredo da Silva <56082029+wilembergson@users.noreply.github.com>
2023-11-19 23:48:36 -0300

    refactor: componentizacao do formulario de enquete

--- src/app/components/Enquete.tsx
+++ src/app/components/Enquete.tsx
@@ -2,28 +2,66 @@
 import { api } from "@/api/api-conections"
 import { useState } from "react"
 
-export default function Enquete() {
+type Props = {
+    visivel: boolean
+}
+
+export default function Enquete({visivel}:Props) {
     const [enquete, setEnquete] = useState<any>(undefined)
+    const [voto, setVoto] = useState('')
+
+    const handleVotoChange = (e: any) => {
+        setVoto(e.target.value);
+    };
 
     async function buscarEnquete() {
         try {
             const res = await api.buscarEnqueteAtiva()
             setEnquete(res.data)
+            console.log('passou')
         } catch (error: any) {
             console.log(error)
         }
     }
     setInterval(() => {
-      buscarEnquete()
+        buscarEnquete()
     }, 5000)
 
+    const estilo = `flex flex-col w-72 px-4`
     return (
         <>
             {enquete ?
-                <div className="flex w-72 px-4">
-                    <h1 className="flex w-hull font-black text-md">
+                <div className={estilo}>
+                    <h1 className="flex w-hull font-black text-md mb-4">
                         {enquete.pergunta}
                     </h1>
+                    <label className="hover:cursor-pointer">
+                        <input className="hover:cursor-pointer"
+                            type="radio"
+                            value="APROVAR"
+                            checked={voto === 'APROVAR'}
+                            onChange={handleVotoChange}
+                        />
+                        APROVAR
+                    </label>
+                    <label className="hover:cursor-pointer">
+                        <input className="hover:cursor-pointer"
+                            type="radio"
+                            value="REPROVAR"
+                            checked={voto === 'REPROVAR'}
+                            onChange={handleVotoChange}
+                        />
+                        REPROVAR
+                    </label>
+                    <label className="hover:cursor-pointer">
+                        <input className="hover:cursor-pointer"
+                            type="radio"
+                            value="ABSTER"
+                            checked={voto === 'ABSTER'}
+                            onChange={handleVotoChange}
+                        />
+                        ABSTER
+                    </label>
                 </div> : <></>
             }
         </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,25 @@
 'use client'
-import { useState } from 'react'
+import { useState } from "react"
 import Player from "./components/palyer"
-import Enquete from './components/Enquete'
+import Enquete from "./components/Enquete"
 
 export default function Home() {
-
+  const [visivel, setVisivel] = useState(false)
   return (
     <main className="flex min-h-screen flex-col items-center p-4">
-      <h1>AGO 2024 - ao vivo</h1>
-      <section className='flex bg-lime-300'>
+      <section className="flex w-full justify-between mb-10 px-52">
+        <h1 className='flex font-black text-4xl'>
+          AGO 2024
+        </h1>
+        <button className="flex font-black text-white items-center p-4 rounded-full
+                bg-green-600 hover:bg-green-500 transition duration-300"
+                onClick={() => setVisivel(true)}>
+          Ir para votação
+        </button>
+      </section>
+      <section className='flex'>
         <Player />
-        <Enquete />
+        <Enquete visivel={visivel} />
       </section>
     </main>
   )
